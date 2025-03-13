816e0fb9b10fcff67c0ff5732120d14ca2bb71a2
harikrishns <harikrishna205jai@gmail.com>
2025-03-13 19:01:23 +0530

    first commit

---
+++ Nativequearys/src/main/java/com/Nativequeary/demo/NativequearysApplication.java
@@ -0,0 +1,13 @@
+package com.Nativequeary.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativequearysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativequearysApplication.class, args);
+	}
+
+}
---
+++ Nativequearys/src/test/java/com/Nativequeary/demo/NativequearysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nativequeary.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativequearysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
